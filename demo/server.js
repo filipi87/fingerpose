998c799eca5a868c34d260e614270ab77b962692
filipi <filipi87@gmail.com>
2021-12-08 08:59:02 -0300

    Configuring demo to run local

---
+++ demo/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const loadConfig = function () {
+  const config = require('./app.json')
+  return config
+}
+
+const start = async () => {
+  const config = loadConfig()
+  const app = express()
+  app.use('/', express.static('./demo', {
+      index: 'index.html'
+  }))
+  app.use('/dist', express.static('./dist'))
+  app.listen(config.port, () => {
+    console.log(`recognition-service listening on port ${config.port}`)
+  })
+}
+start()
+
