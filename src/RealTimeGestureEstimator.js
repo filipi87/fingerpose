be1654dac85b5432fddb9a9d179870d95d3a5d4a
filipi <filipi87@gmail.com>
2021-12-09 10:13:08 -0300

    Just emiting the events when the gesture changes

--- src/RealTimeGestureEstimator.js
+++ src/RealTimeGestureEstimator.js
@@ -9,6 +9,7 @@ export default class RealTimeGestureEstimator extends Events{
   constructor(knownGestures, estimatorOptions = {}) {
     super();
     this.gestureEstimator = new GestureEstimator(knownGestures, estimatorOptions);
+    this.lastGesturePredicted = undefined
   }
 
   async initialize(videoElement) {
@@ -18,21 +19,23 @@ export default class RealTimeGestureEstimator extends Events{
   }
 
   async _estimateHands(){
+    //the handpose model currently only support one hand, so It is always returning one prediction
     const predictions = await this.handposeModel.estimateHands(this.videoElement, true);
-    for(let i = 0; i < predictions.length; i++) {
-      // estimate gestures based on landmarks
+    let currentGesturePrediction = undefined
+    if(predictions.length > 0){
       // using a minimum score of 9 (out of 10)
-      // gesture candidates with lower score will not be returned
-      const est = this.gestureEstimator.estimate(predictions[i].landmarks, 9);
+      const est = this.gestureEstimator.estimate(predictions[0].landmarks, 9);
       if(est.gestures.length > 0) {
         // find gesture with highest match score
         let result = est.gestures.reduce((p, c) => {
           return (p.score > c.score) ? p : c;
         });
-        const gesture = result.name;
-        //TODO we could include business logic to just emit events when the gesture changes
-        this.emit('gesture', gesture);
+        currentGesturePrediction = result.name;
       }
+    }
+    if(this.lastGesturePredicted !== currentGesturePrediction){
+      this.emit('gesture', currentGesturePrediction);
+      this.lastGesturePredicted = currentGesturePrediction
     }
   }
 
